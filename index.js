6d4136f06257fd52027212cf3399a312ffc0d548
Owen Mowery <owenmowery99@gmail.com>
2021-06-09 13:56:24 -0400

    commit tree 877e95a38b3ac4f5b1a8df8acdb0d6a5e357791e

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\-\' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const exemptions = ["of", "the", "a", "an", "and", "but", "for", "at", "by", "from"]
+
+    return string.split(" ").map(function(word, index) { 
+      if (index !== 0 && exemptions.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }.bind(this)).join(" ")
+  }
 }
